7bfdcad0a9b560d810899949d4dc37d7011a5e0f
Kacper Cetera <kacpe@Mac-mini.local>
2025-06-24 11:26:07 +0200

    Solve Exercism: Raindrops

---
+++ raindrops.py
@@ -0,0 +1,14 @@
+def convert(number):
+    sound = ""
+    if number%3 == 0:
+        sound = "Pling"
+    
+    if number%5 == 0:
+        sound = sound + "Plang" 
+    
+    if number%7 == 0:
+        sound = sound + "Plong"
+
+    if number%3 != 0 and number%5 != 0 and number%7 != 0:
+        sound = str(number)
+    return sound
