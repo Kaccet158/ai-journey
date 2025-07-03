7ae8304b416a901a52ac422d0a1749e106828ebe
Kacper Cetera <kacpe@Mac-mini.local>
2025-07-03 12:24:53 +0200

    Feat: Solve Resistor Color exercise

---
+++ resistor_color.py
@@ -0,0 +1,9 @@
+colors = 'black brown red orange yellow green blue violet grey white'
+colors_list = colors.split()
+
+def color_code(color):
+    return colors().index(color)
+
+def colors():
+    return colors_list
+
